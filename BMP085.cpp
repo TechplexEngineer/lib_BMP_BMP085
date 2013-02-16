370ffc829a2c9b34858544b4dd61336b07378d79
Blake Bourque <Techplex.Engineer@gmail.com>
2013-02-15 23:19:38 -0500

    Final touches on library, adding readme

--- BMP085.cpp
+++ BMP085.cpp
@@ -45,7 +45,7 @@ short BarometricPressure::GetTemp(TEMP_MODE tm/*=TEMP_C*/)
     return ut;
 
   long x1, x2;
-  
+
   x1 = (((long)ut - (long)ac6)*(long)ac5) >> 15;
   x2 = ((long)mc << 11)/(x1 + md);
   b5 = x1 + x2;
@@ -75,26 +75,26 @@ long BarometricPressure::GetPressure(PRES_MODE pm/*=PRES_PA*/)
 
   long x1, x2, x3, b3, b6, p;
   unsigned long b4, b7;
-  
+
   b6 = b5 - 4000;
   // Calculate B3
   x1 = (b2 * (b6 * b6)>>12)>>11;
   x2 = (ac2 * b6)>>11;
   x3 = x1 + x2;
   b3 = (((((long)ac1)*4 + x3)<<OSS) + 2)>>2;
-  
+
   // Calculate B4
   x1 = (ac3 * b6)>>13;
   x2 = (b1 * ((b6 * b6)>>12))>>16;
   x3 = ((x1 + x2) + 2)>>2;
   b4 = (ac4 * (unsigned long)(x3 + 32768))>>15;
-  
+
   b7 = ((unsigned long)(up - b3) * (50000>>OSS));
   if (b7 < 0x80000000)
     p = (b7<<1)/b4;
   else
     p = (b7/b4)<<1;
-    
+
   x1 = (p>>8) * (p>>8);
   x1 = (x1 * 3038)>>16;
   x2 = (-7357 * p)>>16;
@@ -114,11 +114,11 @@ char BarometricPressure::Read(unsigned char address)
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(address);
   Wire.endTransmission();
-  
+
   Wire.requestFrom(BMP085_ADDRESS, 1);
   while(!Wire.available())
     ;
-    
+
   return Wire.read();
 }
 
@@ -128,17 +128,17 @@ char BarometricPressure::Read(unsigned char address)
 int BarometricPressure::ReadInt(unsigned char address)
 {
   unsigned char msb, lsb;
-  
+
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(address);
   Wire.endTransmission();
-  
+
   Wire.requestFrom(BMP085_ADDRESS, 2);
   while(Wire.available()<2)
     ;
   msb = Wire.read();
   lsb = Wire.read();
-  
+
   return (int) msb<<8 | lsb;
 }
 
@@ -146,17 +146,17 @@ int BarometricPressure::ReadInt(unsigned char address)
 unsigned int BarometricPressure::ReadUT(void)
 {
   unsigned int ut;
-  
+
   // Write 0x2E into Register 0xF4
   // This requests a temperature reading
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(0xF4);
   Wire.write(0x2E);
   Wire.endTransmission();
-  
+
   // Wait at least 4.5ms
   delay(5);
-  
+
   // Read two bytes from registers 0xF6 and 0xF7
   ut = ReadInt(0xF6);
   return ut;
@@ -167,32 +167,32 @@ unsigned long BarometricPressure::ReadUP(void)
 {
   unsigned char msb, lsb, xlsb;
   unsigned long up = 0;
-  
+
   // Write 0x34+(OSS<<6) into register 0xF4
   // Request a pressure reading w/ oversampling setting
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(0xF4);
   Wire.write(0x34 + (OSS<<6));
   Wire.endTransmission();
-  
+
   // Wait for conversion, delay time dependent on OSS
   delay(2 + (3<<OSS));
-  
+
   // Read register 0xF6 (MSB), 0xF7 (LSB), and 0xF8 (XLSB)
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(0xF6);
   Wire.endTransmission();
   Wire.requestFrom(BMP085_ADDRESS, 3);
-  
+
   // Wait for data to become available
   while(Wire.available() < 3)
     ;
   msb = Wire.read();
   lsb = Wire.read();
   xlsb = Wire.read();
-  
+
   up = (((unsigned long) msb << 16) | ((unsigned long) lsb << 8) | (unsigned long) xlsb) >> (8-OSS);
-  
+
   return up;
 }
 
---
+++ README.md
@@ -0,0 +1,16 @@
+
+Arduino Library for the BMP085 I2C Barometric Pressure Sensor
+----------------------------
+
+# Using the Library
+1. Use the [zip link][1] to download the source
+2. Extract the contents to your arduino libraries folder (mine is in my home directory ~/sketchbook/libaries You might need to create it.)
+3. View the example program to see how to read and set the time.
+
+
+# Credits
+Original source from [Sparkfun.com][2]
+
+
+[1]: https://github.com/TechplexEngineer/lib_BMP_BMP085/archive/master.zip
+[2]: http://www.sparkfun.com/tutorial/Barometric/BMP085_Example_Code.pde
